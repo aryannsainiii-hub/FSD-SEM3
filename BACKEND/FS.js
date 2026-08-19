f9caeba34578176bd93d0f76f24fe045fbd3ebae
aryannsainiii-hub <aryannsainiii@gmail.com>
2026-08-19 11:23:57 +0530

    exp

--- BACKEND/FS.js
+++ BACKEND/FS.js
@@ -1,13 +1,13 @@
 const fs = require("fs");
 
 //create
-fs.writeFileSync("file.txt","hello student");
+fs.writeFileSync("data.txt","hello student");
 
 //read
 const data = fs.readFileSync("data.txt","utf-8");
 
 //update
-fs.appendFileSync("data.txt","hello student");
+fs.appendFileSync("data.txt","hello guys");
 
 //read updated data
 console.log(fs.readFileSync("data.txt","utf-8"));
@@ -16,3 +16,15 @@ console.log(fs.readFileSync("data.txt","utf-8"));
 fs.unlinkSync("data.txt");
 
 console.log("file deleted");
+
+
+//test 
+const testfile = require("testfile");
+
+testfile.createFile("test.txt","file has been created");
+
+const data = readFileSync("test.txt","utf-8");
+fs.appendFileSync("test.txt","file has been appended");
+console.log(fs.readFileSync("test.txt","utf-8"));
+
+console.log(fs.readFileSync("test.txt","utf-8"));
